db79d305d1e151eda994236e53a267a42b80bf3c
ThiboVerstraete <146202098+ThiboVerstraete@users.noreply.github.com>
2024-12-16 08:55:42 +0100

    Initial commit

---
+++ PeopleManager.Model/Person.cs
@@ -0,0 +1,24 @@
+﻿using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace PeopleManager.Model
+{
+    [Table(nameof(Person))]
+    public class Person
+    {
+        public int Id { get; set; }
+
+        [Required]
+        [DisplayName("First name")]
+        public required string FirstName { get; set; }
+
+        [Required]
+        [DisplayName("Last name")]
+        public required string LastName { get; set; }
+
+        [DisplayName("E-mail address")]
+        [EmailAddress]
+        public string? Email { get; set; }
+    }
+}
---
+++ PeopleManager.Ui.Mvc/Controllers/PeopleController.cs
@@ -0,0 +1,117 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using PeopleManager.Ui.Mvc.Core;
+using PeopleManager.Model;
+
+namespace PeopleManager.Ui.Mvc.Controllers
+{
+    public class PeopleController : Controller
+    {
+
+        private readonly PeopleManagerDbContext _peopleManagerDbContext;
+
+        public PeopleController(PeopleManagerDbContext peopleManagerDbContext)
+        {
+            _peopleManagerDbContext = peopleManagerDbContext;
+        }
+
+        [HttpGet]
+        public IActionResult Index()
+        {
+            var people = _peopleManagerDbContext.People.ToList();
+
+            return View(people);
+        }
+
+        [HttpGet]
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult Create(Person person)
+        {
+            if (!ModelState.IsValid)
+            {
+                return View(person);
+            }
+
+            _peopleManagerDbContext.People.Add(person);
+
+            _peopleManagerDbContext.SaveChanges();
+
+            return RedirectToAction("Index");
+        }
+
+        [HttpGet]
+        public IActionResult Edit([FromRoute]int id)
+        {
+            var person = _peopleManagerDbContext.People.FirstOrDefault(p => p.Id == id);
+            if (person is null)
+            {
+                return RedirectToAction("Index");
+            }
+            
+            return View(person);
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult Edit([FromRoute]int id, [FromForm]Person person)
+        {
+            if (!ModelState.IsValid)
+            {
+                return View(person);
+            }
+
+            var dbPerson = _peopleManagerDbContext.People.FirstOrDefault(p => p.Id == id);
+            if (dbPerson is null)
+            {
+                return RedirectToAction("Index");
+            }
+
+            dbPerson.FirstName = person.FirstName;
+            dbPerson.LastName = person.LastName;
+            dbPerson.Email = person.Email;
+
+            _peopleManagerDbContext.SaveChanges();
+
+            return RedirectToAction("Index");
+        }
+
+        [HttpGet]
+        public IActionResult Delete([FromRoute] int id)
+        {
+            var person = _peopleManagerDbContext.People.FirstOrDefault(p => p.Id == id);
+            if (person is null)
+            {
+                return RedirectToAction("Index");
+            }
+
+            return View(person);
+        }
+
+        [HttpPost]
+        [Route("People/Delete/{id:int?}")]
+        [ValidateAntiForgeryToken]
+        public IActionResult DeleteConfirmed(int id)
+        {
+            var person = new Person
+            {
+                Id = id,
+                FirstName = string.Empty,
+                LastName = string.Empty
+            };
+
+            _peopleManagerDbContext.People.Attach(person);
+
+            _peopleManagerDbContext.People.Remove(person);
+
+            _peopleManagerDbContext.SaveChanges();
+
+            return RedirectToAction("Index");
+        }
+    }
+}
+
